d7a44754910ac4d29a9e647704218b92f2d422d5
enhanceoffice <re_sute@yahoo.co.jp>
2024-04-30 20:53:47 +0900

    arrayToSelectに$class引数追加

--- functions.php
+++ functions.php
@@ -5,9 +5,9 @@ require_once(dirname(__FILE__).'/../config/config.php');
 //プルダウン箇所は比較処理が必要な為、共通関数を制作
 
 //引数で与えられた配列を元にプルダウンリストを自動生成
-function arrayToSelect($inputName, $srcArray, $selectedIndex= "")
+function arrayToSelect($inputName, $srcArray, $selectedIndex= "", $class = "form-select")
 {
-    $temphtml ="<select class=\"form-select\" name=\"{$inputName}\">" .PHP_EOL;
+    $temphtml ="<select class=\"{$class}\" name=\"{$inputName}\">" .PHP_EOL;
     
     foreach($srcArray as $key => $val){
         //キーと選択値を比較して一致したらselectedを付与
